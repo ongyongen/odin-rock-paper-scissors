fcc4d8a8c128872a86d0d42cb05b7227c9ffb79a
ongyongen <ongyongen2021@gmail.com>
2022-05-30 02:53:28 +0800

    refactor code to include a GUI

--- scripts.js
+++ scripts.js
@@ -1,3 +1,76 @@
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Buttons for user to choose between rock / paper / scissors
+const choiceButtons = document.querySelectorAll("button#choice")
+choiceButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let playerChoice = button.textContent
+        let computerChoice = computerPlay()
+
+        let playerSelectionElem = document.querySelector('#player-selection')
+        playerSelectionElem.textContent = `Your selection: ${playerChoice}`
+
+        let computerSelectionElem = document.querySelector('#computer-selection')
+        computerSelectionElem.textContent = `Computer selection: ${computerChoice}`
+
+        let winner = playRound(playerChoice, computerChoice)
+        let winnerElem = document.querySelector('#round-winner')
+        winnerElem.textContent = `Round winner: ${winner}`
+
+        updateScoreboard(winner)
+
+        if (playerScore == 5) {
+            declareFinalWinner('Player')
+        }
+
+        if (computerScore == 5) {
+            declareFinalWinner('Computer')
+        }
+    })
+})
+
+// Reset game board
+const resetButton = document.querySelector('button#reset')
+resetButton.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+
+    let playerSelectionElem = document.querySelector('#player-selection')
+    playerSelectionElem.textContent = `Your selection:`
+
+    let computerSelectionElem = document.querySelector('#computer-selection')
+    computerSelectionElem.textContent = `Computer selection:`
+
+    let winnerElem = document.querySelector('#round-winner')
+    winnerElem.textContent = `Round winner:`
+
+    let playerScoreElem = document.querySelector('#player-total-wins')
+    playerScoreElem.textContent = 'Nos of rounds you won:'
+
+    let computerScoreElem = document.querySelector('#computer-total-wins')
+    computerScoreElem.textContent = 'Nos of rounds computer won:'
+})
+
+// Update score tally after each round
+function updateScoreboard(winner) {
+    if (winner == 'Player') {
+        playerScore += 1;
+        let playerScoreElem = document.querySelector('#player-total-wins')
+        playerScoreElem.textContent = `Nos of rounds you won: ${playerScore}`
+    } else if (winner == 'Computer') {
+        computerScore += 1;
+        let computerScoreElem = document.querySelector('#computer-total-wins')
+        computerScoreElem.textContent = `Nos of rounds computer won: ${computerScore}`
+    }
+}
+
+// Raise an alert for the overall winner after 5 rounds
+function declareFinalWinner(winner) {
+    alert(`${winner} has won the game!`)
+}
+
 // Returns a randomised choice of rock / paper / scissors 
 function computerPlay() {
     const choices = ['rock', 'paper', 'scissors']
@@ -5,37 +78,17 @@ function computerPlay() {
     return choices[index]
 }
 
-// Prompts users to make a selection
-function playerPlay() {
-    let playerSelection = prompt("Enter your choice")
-    return playerSelection
-}
-
 // Complete 1 play round
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection == computerSelection) {
-        console.log('It is a draw!')
-        return 'draw'
+        return 'It is a draw!'
     } else if ( playerSelection == 'rock' && computerSelection == 'scissors' || 
                 playerSelection == 'scissors' && computerSelection == 'paper' ||
                 playerSelection == 'paper' && computerSelection == 'rock' ) {
-        console.log('Player wins!')
-        return 'player'
+        return 'Player'
     } else {
-        console.log('Computer wins!')
-        return 'computer'
+        return 'Computer'
     }
 }
 
-// Complete a game of 5 rounds
-function play() {
-    const winners = []
-    for (let i = 0; i < 5; i++) {
-        let winner = playRound(playerPlay(), computerPlay())
-        winners.push(winner)
-    }
-    numPlayerWins = winners.filter((winner) => winner == 'player')
-    numComputerWins = winners.filter((winner) => winner == 'computer')
-    return numPlayerWins > numComputerWins ? 'You win!' : 'Computer wins!'
-}
